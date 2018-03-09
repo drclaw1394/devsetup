f4fbb1da42a6bc3cb2e5959f9730436a5e9b0f70
Ruben <rwesterberg@dgrt.com.au>
2018-03-09 02:53:26 +0000

    Added tmux config
    Copies the tmux config to user home directort

--- devsetup.sh
+++ devsetup.sh
@@ -3,16 +3,22 @@
 
 OS=$(uname )
 
+echo Updating and installing packages: tmux, vim, boxes, slurm, etc
 if [ $OS = "Darwin" ]; then
-	sudo port install vim boxes slurm
+	sudo port sync
+	sudo port install -N tmux vim boxes slurm zsh
 fi
 
 if [ $OS = "Linux" ]; then
-	sudo apt-get install vim boxes slurm
+	sudo apt-get update
+	sudo apt-get install -y  tmux vim boxes slurm zsh
 fi
 
 if [ $OS = "FreeBSD" ]; then
-	sudo pkg install vim boxes slurm
+	sudo pkg install tmux vim boxes slurm zsh
 fi
+
+echo Copying tmux config to home directory
+cp .tmux.conf $HOME/.tmux.conf	 
 
 
