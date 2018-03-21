404ab602cb46aff66104f504d685cde3079affa2
Ruben <rwesterberg@dgrt.com.au>
2018-03-21 12:53:34 +0000

    Added list gits source

--- devsetup.sh
+++ devsetup.sh
@@ -6,16 +6,16 @@ OS=$(uname )
 echo Updating and installing packages: tmux, vim, boxes, slurm, etc
 if [ $OS = "Darwin" ]; then
 	sudo port sync
-	sudo port install -N tmux vim boxes slurm zsh
+	sudo port install -N tmux vim boxes slurm zsh socat
 fi
 
 if [ $OS = "Linux" ]; then
 	sudo apt-get update
-	sudo apt-get install -y  tmux vim boxes slurm zsh
+	sudo apt-get install -y  tmux vim boxes slurm zsh socat 
 fi
 
 if [ $OS = "FreeBSD" ]; then
-	sudo pkg install tmux vim boxes slurm zsh
+	sudo pkg install tmux vim boxes slurm zsh socat
 fi
 
 echo Copying tmux config to home directory
